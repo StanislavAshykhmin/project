3dce2672b38764f9255f23e740030aff8caa2666
123Stepan <stepankiryanenko@gmail.com>
2018-12-10 13:42:58 +0200

    login.blade.php

--- project/resources/views/login.blade.php
+++ project/resources/views/login.blade.php
@@ -1,26 +1,4 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="R9nIuIOgs3vinojjbuCgDEAFVoFJjBpqbeZO0k3J">
-
-    <title>PolyMath</title>
-
-    <!-- Scripts -->
-    <script src="js/app.js" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link href="css/style-login.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat|Muli" rel="stylesheet">
-</head>
 <body>
     <div id="app">
             <div class="container">
@@ -51,5 +29,5 @@
         </main>
     </div>
 </body>
-</html>
+
 
