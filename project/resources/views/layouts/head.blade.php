300d138ddbb2a0e9b617a3a168bd38d0d145ccad
StanislavAshykhmin <ashykhminstanislav@gmail.com>
2018-12-10 16:25:35 +0200

    created user login

--- project/resources/views/layouts/head.blade.php
+++ project/resources/views/layouts/head.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="R9nIuIOgs3vinojjbuCgDEAFVoFJjBpqbeZO0k3J">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>PolyMath</title>
 
--- project/resources/views/login.blade.php
+++ project/resources/views/login.blade.php
@@ -6,22 +6,43 @@
                 <a class="navbar-brand" href="#"><img src="img/logo.png" align="center"><span class="Poly">PolyMath</span></a>
             </div>
 
-             <form name="test" method="post" action="input1.php">
+
   
         <main class="py-4">
             <div class="row">
                 <p class="card-header">Log In</p>
-                <form method="POST" action="http://localhost:45001/login">
-                <input type="hidden" name="_token" value="R9nIuIOgs3vinojjbuCgDEAFVoFJjBpqbeZO0k3J">
+                <form method="POST" action="{{ route('login') }}">
+                    {{ csrf_field() }}
+                {{--<input type="hidden" name="_token" value="R9nIuIOgs3vinojjbuCgDEAFVoFJjBpqbeZO0k3J">--}}
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+
                 <label for="email" class="col-sm-4 col-form-label text-md-right"></label>
-                <input id="email" type="email" class="form-control" name="email" value="" required autofocus placeholder="Username">
+
+                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus placeholder="Username">
+                        <div class="col-md-6">
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                 <label for="password" class="col-md-4 col-form-label text-md-right"></label>
+                        <div class="col-md-6">
                 <input id="password" type="password" class="form-control" name="password" required placeholder="Password"><br>
+                            @if ($errors->has('password'))
+                                <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                            @endif
+                        </div>
+                    </div>
                 <button type="submit" class="btn btn-primary">
                     Log In
                 </button><br>
                 <div class="btn-link">
-                    <a href="#">
+                    <a href="{{ route('password.request') }}">
                         Forgot Password?
                     </a>
                 </div>
